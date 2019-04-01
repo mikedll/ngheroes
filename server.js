10cfb9914f8dba234c17fb63c0938cb591062be2
Michael Rivera <soymrmike@gmail.com>
2019-03-31 20:05:53 -0700

    Node Express server can serve the angular files.

---
+++ server.js
@@ -0,0 +1,12 @@
+
+const express = require('express')
+const app = express()
+
+const port = 3000
+
+app.use(express.static('dist/stays5'))
+
+// app.get('/', (req, res) => res.send("Hello World!"))
+
+app.listen(port, () => console.log(`Listening on port ${port}!`))
+
