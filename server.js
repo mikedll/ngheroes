bf9d7206074baff590a375aef447b709be833325
Michael Rivera <soymrmike@gmail.com>
2019-04-01 18:29:13 -0700

    Configuration for app. Installed joi, mongoose.

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 
+const config = require('./server/config')
 const express = require('express')
 const app = express()
 
-const port = 3000
+const port = config.port
 
 app.use(express.static('dist/stays5'))
 
 // app.get('/', (req, res) => res.send("Hello World!"))
 
-app.listen(port, () => console.log(`Listening on port ${port}!`))
+// app.listen(port, () => console.log(`Listening on port ${port}!`))
 
